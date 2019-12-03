ed448146f61acf6353030b4f3e86147ff23981f6
Daniel Lenski <dlenski@gmail.com>
2019-12-03 14:36:07 -0500

    use latest ZXing release (3.4.0)

--- setup.py
+++ setup.py
@@ -37,8 +37,8 @@ else:
 ########################################
 
 def download_java_files():
-    files = {'java/javase.jar': 'https://repo1.maven.org/maven2/com/google/zxing/javase/3.3.1/javase-3.3.1.jar',
-             'java/core.jar': 'https://repo1.maven.org/maven2/com/google/zxing/core/3.3.1/core-3.3.1.jar',
+    files = {'java/javase.jar': 'https://repo1.maven.org/maven2/com/google/zxing/javase/3.4.0/javase-3.4.0.jar',
+             'java/core.jar': 'https://repo1.maven.org/maven2/com/google/zxing/core/3.4.0/core-3.4.0.jar',
              'java/jcommander.jar': 'https://repo1.maven.org/maven2/com/beust/jcommander/1.72/jcommander-1.72.jar'}
 
     for fn, url in files.items():
