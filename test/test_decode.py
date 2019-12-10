70c65f9fb1c722150f2c89c328036b9ab85c4ec7
Daniel Lenski <dlenski@gmail.com>
2019-12-10 15:08:53 -0800

    add tests for BarCodeReaderExceptions

--- test/test_decode.py
+++ test/test_decode.py
@@ -1,6 +1,10 @@
 import logging
 import zxing
+from tempfile import mkdtemp
 import os
+
+from nose import with_setup
+from nose.tools import raises
 
 test_barcode_dir = os.path.join(os.path.dirname(__file__), 'barcodes')
 
@@ -9,19 +13,29 @@ test_barcodes = [
     ( 'CODE_128-easy.jpg', 'This should be CODE_128' ),
     ( 'PDF_417-easy.bmp', 'This should be PDF_417' ),
     ( 'AZTEC-easy.jpg', 'This should be AZTEC' ),
+    ( 'QR CODE (¡filenáme törture test! 😉).png', 'This should be QR_CODE' ),
+    ( 'QR_CODE-png-but-wrong-extension.bmp', 'This should be QR_CODE' ),
     ( 'CODE_128-fun-with-whitespace.png', '\n\r\t\r\r\r\n ' ),
     ( 'QR_CODE-screen_scraping_torture_test.png',
       '\n\\n¡Atención ☹! UTF-8 characters,\n embedded newline, &amp; trailing whitespace\t ' ),
 ]
 
+test_reader = None
+def setup_reader():
+    global test_reader
+    if test_reader is None:
+        test_reader = zxing.BarCodeReader()
+
+@with_setup(setup_reader)
 def test_decoding():
-    reader = zxing.BarCodeReader()
+    global test_reader
     for filename, expected in test_barcodes:
         path = os.path.join(test_barcode_dir, filename)
         logging.debug('Trying to parse {}, expecting {!r}.'.format(path, expected))
-        dec = reader.decode(path)
+        dec = test_reader.decode(path)
         if dec.parsed != expected:
             raise AssertionError('Expected {!r} but got {!r}'.format(expected, dec.parsed))
+
 
 def test_parsing():
     dec = zxing.BarCode.parse("""
@@ -42,3 +56,29 @@ Found 4 result points:
     assert dec.raw == 'Élan|\tthe barcode is taking off'
     assert dec.parsed == 'Élan\n\tthe barcode is taking off'
     assert dec.points == [(24.0,18.0),(21.0,196.0),(201.0,198.0),(205.23952,21.0)]
+
+
+@raises(zxing.BarCodeReaderException)
+def test_bad_java():
+    test_reader = zxing.BarCodeReader(java=os.devnull)
+    test_reader.decode(test_barcodes[0][0])
+
+
+@raises(zxing.BarCodeReaderException)
+def test_bad_classpath():
+    test_reader = zxing.BarCodeReader(classpath=mkdtemp())
+    test_reader.decode(test_barcodes[0][0])
+
+
+@raises(zxing.BarCodeReaderException)
+@with_setup(setup_reader)
+def test_nonexistent_file_error():
+    global test_reader
+    test_reader.decode(os.path.join(test_barcode_dir, 'nonexistent.png'))
+
+
+@raises(zxing.BarCodeReaderException)
+@with_setup(setup_reader)
+def test_bad_file_format_error():
+    global test_reader
+    test_reader.decode(os.path.join(test_barcode_dir, 'bad_format.png'))
